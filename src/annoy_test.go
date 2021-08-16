0bf8b7081890a9f67e6ba38061ce68a039bef1fe
m10an <meleshkoi@email.loc>
2021-08-16 10:26:48 +0300

    Added more things to test

--- src/annoy_test.go
+++ src/annoy_test.go
@@ -5,6 +5,8 @@ import "fmt"
 
 func TestCreateAndFree(t *testing.T) {
 	index := NewAnnoyIndexAngular(3)
+	index.Verbose(true)
+	index.OnDiskBuild("index.ann")
 	v1 := []float32{0, 0, 1}
 	v2 := []float32{0, 1, 0}
 	v3 := []float32{1, 0, 0}
